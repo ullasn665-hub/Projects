fbf8ffa4002abcd145aad044cfab163b53cfdc46
ullasn665-hub <ullasn665@gmail.com>
2026-08-17 22:12:35 +0530

    Initial commit

---
+++ src/IOT_TRAINING/division.java
@@ -0,0 +1,13 @@
+package IOT_TRAINING;
+
+public class division {
+	public static void main(String[] args){
+		int a=100;
+		int b=20;
+		int result=a/b;
+		System.out.println("Division:"+result);
+		
+		
+	}
+
+}
