93a5d1242a1edb5bc915c4d9cd85f4d0ba56eaed
ullasn665-hub <ullasn665@gmail.com>
2026-08-18 07:40:30 +0530

    day01 ATM

---
+++ src/day01.java
@@ -0,0 +1,68 @@
+package ATM;
+import java.util.Scanner;
+public class day01 {
+        String name;
+	    int age;
+	    String job;
+
+	    void create() {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter name: ");
+	        name = sc.nextLine();
+
+	        System.out.print("Enter age: ");
+	        age = sc.nextInt();
+	        sc.nextLine();
+
+	        System.out.print("Enter job: ");
+	        job = sc.nextLine();
+	    switch (job) {
+	    case "Developer":
+	    case "Manager":
+	    case "Tester":
+	        System.out.println("Account Created");
+	        break;
+
+	    default:
+	        System.out.println("Invalid job");
+	}
+
+}
+
+	    public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+	        day01 atm = new day01();
+
+	        int choice;
+
+	        do {
+	            System.out.println("\n===== ATM =====");
+	            System.out.println("1. Create Account");
+	            System.out.println("2. Exit");
+	            System.out.print("Enter choice: ");
+
+	            choice = sc.nextInt();
+	            sc.nextLine();
+
+	            switch (choice) {
+
+	                case 1:
+	                    atm.create();
+	                    break;
+
+	                case 2:
+	                    System.out.println("Thank you!");
+	                    break;
+
+	                default:
+	                    System.out.println("Invalid choice");
+	            }
+
+	        } while (choice != 2);
+
+	    }
+	}
+	
